dc4f11d4287a24053eca4b68199ed325afcd4d10
sicmmar <sicmariana8@gmail.com>
2020-09-17 22:32:25 -0600

    tablero inicial

--- P3/macros.asm
+++ P3/macros.asm
@@ -7,21 +7,22 @@ print macro cadena
 endm
 
 ObtenerTexto macro buffer
+local CONTINUE, FIN
     PUSH SI
     PUSH AX
 
     xor si,si
     CONTINUE:
-    getChar
-    cmp al,0dh
-    je FIN
-    mov buffer[si],al
-    inc si
-    jmp CONTINUE
+        getChar
+        cmp al,0dh
+        je FIN
+        mov buffer[si],al
+        inc si
+        jmp CONTINUE
 
     FIN:
-    mov al,'$'
-    mov buffer[si],al
+        mov al,'$'
+        mov buffer[si],al
 
     POP AX
     POP SI
@@ -38,4 +39,187 @@ printChar macro char
     mov ah,02h
     mov dl,char
     int 21h
+endm
+
+llenarInicial macro b1,b2,b3,n1,n2,n3
+    mov b1[0],11b
+    mov b1[2],11b
+    mov b1[4],11b
+    mov b1[6],11b
+    mov b2[1],11b
+    mov b2[3],11b
+    mov b2[5],11b
+    mov b2[7],11b
+    mov b3[0],11b
+    mov b3[2],11b
+    mov b3[4],11b
+    mov b3[6],11b
+    mov n1[0],10b
+    mov n1[2],10b
+    mov n1[4],10b
+    mov n1[6],10b
+    mov n2[1],10b
+    mov n2[3],10b
+    mov n2[5],10b
+    mov n2[7],10b
+    mov n3[0],10b
+    mov n3[2],10b
+    mov n3[4],10b
+    mov n3[6],10b
+endm
+
+imprimirTablero macro p8,p7,p6,p5,p4,p3,p2,p1,arr,g
+    cleanArr arr
+    mov bl,'8'
+    llenarArr arr,p8
+    print arr
+    print g
+
+    cleanArr arr
+    mov bl,'7'
+    llenarArr arr,p7
+    print arr
+    print g
+
+    cleanArr arr
+    mov bl,'6'
+    llenarArr arr,p6
+    print arr
+    print g
+
+    cleanArr arr
+    mov bl,'5'
+    llenarArr arr,p5
+    print arr
+    print g
+
+    cleanArr arr
+    mov bl,'4'
+    llenarArr arr,p4
+    print arr
+    print g
+
+    cleanArr arr
+    mov bl,'3'
+    llenarArr arr,p3
+    print arr
+    print g
+
+    cleanArr arr
+    mov bl,'2'
+    llenarArr arr,p2
+    print arr
+    print g
+
+    cleanArr arr
+    mov bl,'1'
+    llenarArr arr,p1
+    print arr
+    print g
+endm
+
+llenarArr macro arr,entrada
+    local CONTINUE, FIN, FB, FN, RB, RN, SPACE
+    mov arr[0],bl
+    mov arr[1],9
+    mov arr[2],179
+    
+    mov di,3
+    mov si,0
+    CONTINUE:
+        cmp si,8
+        je FIN
+        mov al,entrada[si]
+        cmp al,11b
+        je FB
+        cmp al,10b
+        je FN
+        cmp al,01b
+        je RB
+        cmp al,00b
+        je RN
+        cmp al,32
+        je SPACE
+    
+    FB:
+        mov arr[di],'F'
+        inc di
+        mov arr[di],'B'
+        inc di
+        mov arr[di],179
+        inc di
+        inc si
+        jmp CONTINUE
+
+    FN:
+        mov arr[di],'F'
+        inc di
+        mov arr[di],'N'
+        inc di
+        mov arr[di],179
+        inc di
+        inc si
+        jmp CONTINUE
+
+    RB:
+        mov arr[di],'R'
+        inc di
+        mov arr[di],'B'
+        inc di
+        mov arr[di],179
+        inc di
+        inc si
+        jmp CONTINUE
+
+    RN:
+        mov arr[di],'R'
+        inc di
+        mov arr[di],'N'
+        inc di
+        mov arr[di],179
+        inc di
+        inc si
+        jmp CONTINUE
+
+    SPACE:
+        mov arr[di],32
+        inc di
+        mov arr[di],32
+        inc di
+        mov arr[di],179
+        inc di
+        inc si
+        jmp CONTINUE
+
+    FIN:
+        mov al,10
+        mov arr[di],al
+        inc di
+        mov al,13
+        mov arr[di],al
+        inc di
+        mov al,'$'
+        mov arr[di],al
+endm
+
+cleanArr macro arr
+    local CONTINUE, FIN
+    PUSH SI
+    PUSH AX
+
+    xor si,si
+    CONTINUE:
+        cmp si,29
+        je FIN
+        mov al,32
+        mov arr[si],al
+        inc si
+        jmp CONTINUE
+
+    FIN:
+        mov al,'$'
+        mov arr[si],al
+
+    POP AX
+    POP SI
 endm
--- P3/main.asm
+++ P3/main.asm
@@ -3,10 +3,21 @@ include macros.asm
 .model small ;declaracion del programa
 .stack ;segmento de stack
 .data ;segmento de datos
-arreglo db 20 dup('$'),'$'
-encab db 'UNIVERSIDAD DE SAN CARLOS DE GUATEMALA',10,13,'FACULTAD DE INGENIERIA',10,13,'CIENCIAS Y SISTEMAS',10,13,'ARQUITECTURA DE COMPUTADORES Y ENSAMBLADORES 1',10,13,'NOMBRE: ASUNCION MARIANA SIC SOR',10,13,'CARNET: 201504051',10,13,'SECCION: A',10,13,10,13,'1. Iniciar Juego',10,13,'2. Cargar Juego',10,13,'3. Salir',10,13,10,'::Escoga una opci',162,'n  ','$'
+arregloAux db 39 dup('$'),'$'
+encab db 10,13,'UNIVERSIDAD DE SAN CARLOS DE GUATEMALA',10,13,'FACULTAD DE INGENIERIA',10,13,'CIENCIAS Y SISTEMAS',10,13,'ARQUITECTURA DE COMPUTADORES Y ENSAMBLADORES 1',10,13,'NOMBRE: ASUNCION MARIANA SIC SOR',10,13,'CARNET: 201504051',10,13,'SECCION: A',10,13,10,13,'1. Iniciar Juego',10,13,'2. Cargar Juego',10,13,'3. Salir',10,13,10,'::Escoga una opci',162,'n  ','$'
 msjTBlancas db 10,13,'::Turno Blancas ','$'
 msjTNegras db 10,23,'::Turno Negras ','$'
+guiones db 32,9,'-------------------------',10,13,'$'
+abc db 32,9,' A  B  C  D  E  F  G  H  ',10,13,'$'
+saltoln db 10,13,'$'
+pos1 db 9 dup(32), '$'
+pos2 db 9 dup(32), '$'
+pos3 db 9 dup(32), '$'
+pos4 db 9 dup(32), '$'
+pos5 db 9 dup(32), '$'
+pos6 db 9 dup(32), '$'
+pos7 db 9 dup(32), '$'
+pos8 db 9 dup(32), '$'
 .code	
 	
 main proc
@@ -22,26 +33,20 @@ main proc
 		jmp MenuPrincipal
 
 	INICIOJUEGO:
-		mov dh,arreglo[3]
-		printChar dh
+		llenarInicial pos8,pos7,pos6,pos3,pos2,pos1
+		print saltoln
+		print abc
+		print guiones
+		imprimirTablero pos8,pos7,pos6,pos5,pos4,pos3,pos2,pos1,arregloAux,guiones
+		print abc
+		print msjTBlancas
 		getChar
 		jmp MenuPrincipal
 
-	MOSTRAR2:
+	CARGAJUEGO:
 		xor si,si
-		mov dh,arreglo[si]
+		mov dh,arregloAux[si]
 		printChar dh
-		getChar
-		jmp MenuPrincipal
-
-	MOSTRAR:
-		print arreglo
-		getChar
-		jmp MenuPrincipal
-
-	INGRESAR:
-		ObtenerTexto arreglo
-
 		getChar
 		jmp MenuPrincipal
 

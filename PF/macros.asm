56e47eed2120876c2fd4e686d61ca3f9e50eeebe
sicmmar <sicmariana8@gmail.com>
2020-10-20 22:43:28 -0600

    BEGINNING

---
+++ PF/macros.asm
@@ -0,0 +1,566 @@
+;include reporte.asm
+
+print macro cadena
+    mov ah,09h
+    lea dx,cadena
+    int 21h
+endm
+
+ObtenerTexto macro buffer
+    local CONTINUE, FIN
+        PUSH SI
+        PUSH AX
+
+        xor si,si
+        CONTINUE:
+            cmp si,7
+            je FIN
+
+            getChar
+            cmp al,32
+            je FIN
+            
+            mov buffer[si],al
+            inc si
+            jmp CONTINUE
+
+        FIN:
+            mov al,'$'
+            mov buffer[si],al
+
+        POP AX
+        POP SI
+endm
+
+ObtenerPwd macro buffer
+    local CONTINUE, FIN, INVALIDO
+        PUSH SI
+        PUSH AX
+
+        xor si,si
+        mov ch,01h
+        CONTINUE:
+            getPwd
+            cmp si,4
+            jge FIN
+
+            cmp al,48
+            jl INVALIDO
+            cmp al,57
+            jg INVALIDO
+
+            mov buffer[si],al
+            mostAsts
+            inc si
+            jmp CONTINUE
+
+        INVALIDO:
+            print contraNumerica
+            mov ch,00h
+        FIN:
+            mov al,'$'
+            mov buffer[si],al
+
+        POP AX
+        POP SI
+endm
+
+mostAsts macro
+    mov ah,06h
+    mov dl,'*'
+    int 21h
+endm
+
+getPwd macro
+    mov ah,08h
+    int 21h
+endm
+
+getChar macro
+    mov ah,01h
+    int 21h
+endm
+
+printChar macro char
+    push dx
+    mov ah,02h
+    mov dl,char
+    int 21h
+    pop dx
+endm
+
+cleanArr macro arr
+    local CONTINUE, FIN
+    PUSH SI
+    PUSH AX
+    push bx
+    push cx
+
+    xor si,si
+    tamanoArr arr
+    mov bx,cx
+    CONTINUE:
+        cmp si,bx
+        je FIN
+        mov al,'$'
+        mov arr[si],al
+        inc si
+        jmp CONTINUE
+
+    FIN:
+        mov al,'$'
+        mov arr[si],al
+
+    pop cx
+    pop bx
+    POP AX
+    POP SI
+endm
+
+cleanArrWord macro arr
+    local CONTINUE, FIN
+    PUSH SI
+    PUSH AX
+    push cx
+
+    xor si,si
+    CONTINUE:
+        cmp si,50
+        je FIN
+        mov ax,00h
+        mov arr[si],ax
+        inc si
+        inc si
+        jmp CONTINUE
+
+    FIN:
+        mov ax,00h
+        mov arr[si],ax
+
+    pop cx
+    POP AX
+    POP SI
+endm
+
+ConvertirString macro buffer
+	LOCAL Dividir,Dividir2,FinCr3,NEGATIVO,FIN2,FIN
+    push si
+    push cx 
+    push bx
+    push dx
+    push ax
+
+	xor si,si
+	xor cx,cx
+	xor bx,bx
+	xor dx,dx
+
+	mov bx,0ah
+	cmp ax,0
+	jl NEGATIVO
+	jmp Dividir2
+
+	NEGATIVO:
+		neg ax
+		mov buffer[si],45
+		inc si
+		jmp Dividir2
+
+	Dividir:
+		xor dx,dx
+	Dividir2:
+		div bx
+		inc cx
+		push dx
+		cmp ax,00h
+		je FinCr3
+		jmp Dividir
+	FinCr3:
+		pop dx
+		add dx,30h
+		mov buffer[si],dx
+		inc si
+		loop FinCr3
+		mov dx,24h
+		mov buffer[si],dx
+		inc si
+	FIN:
+        pop ax
+        pop dx
+        pop bx
+        pop cx
+        pop si
+endm
+
+ConvertirAscii macro numero
+	LOCAL INICIO,FIN, NEGATIVO, NEGAT,FINEG
+
+    push si
+    push bx
+	xor ax,ax
+	xor bx,bx
+
+	mov bx,10	;multiplicador 10
+	xor si,si
+	INICIO:
+	    xor cx,cx
+		mov cl,numero[si] 
+		cmp cl,45
+		je NEGATIVO
+		cmp cl,48
+		jl FIN
+		cmp cl,57
+		jg FIN
+		inc si
+		sub cx,48	;restar 48 para que me de el numero
+		mul bx		;multplicar ax por 10
+		add ax,cx	;sumar lo que tengo mas el siguiente
+		jmp INICIO
+
+	NEGATIVO:
+		inc si
+		jmp NEGAT
+
+	NEGAT:
+		mov cl,numero[si]
+		cmp cl,48
+		jl FINEG
+		cmp cl,57
+		jg FINEG
+		inc si
+		sub cl,48
+		mul bx		;multplicar ax por 10
+		add ax,cx	;sumar lo que tengo mas el siguiente
+		jmp NEGAT
+
+	FINEG:
+		mov cx,ax
+		mov bx,2
+		mul bx
+		sub cx,ax
+		mov ax,cx
+
+
+	FIN:
+        pop bx
+        pop si
+endm
+
+ConvertirSumaAscii macro numero
+	LOCAL INICIO,FIN
+
+    push si
+    xor ax,ax
+
+	xor si,si
+	INICIO:
+	    xor cx,cx
+		mov cl,numero[si] 
+		cmp cl,'$'
+		je FIN
+
+        add ax,cx
+		inc si
+		jmp INICIO
+
+	FIN:
+        pop si
+endm
+
+colocarRespuesta macro arreglo
+    local INICIO, FIN
+
+    push si
+    push bx
+    xor si,si
+
+    INICIO:
+        mov bx,arreglo[si]
+        cmp bx,00h
+        je FIN
+
+        inc si
+        inc si
+        jmp INICIO
+
+    FIN:
+        mov arreglo[si],ax
+    
+    pop bx
+    pop si
+
+endm
+
+searchID macro
+    ;lo que devuelve el id, se coloca en dx
+    local INICIO, NOENCONTRADO, ENCONTRADO, FIN
+    push si
+    push bx
+    xor si,si
+
+    INICIO:
+        mov bx,usuarios[si]
+        cmp bx,00h
+        je NOENCONTRADO
+        cmp bx,ax
+        je ENCONTRADO
+
+        inc si
+        inc si
+        jmp INICIO
+    
+    NOENCONTRADO:
+        mov dx,0
+        jmp FIN
+    
+    ENCONTRADO:
+        mov dx,contrasenas[si]
+
+    FIN:
+
+    pop bx 
+    pop si
+endm
+
+colocarIdentificador macro arreglo
+    local INICIO, FIN, FIN2
+    push si
+    push ax
+    push di
+
+    xor si,si
+    xor ax,ax
+    xor di,di
+    
+    INICIO:
+        mov  al,nombresIdentificadores[si]
+        cmp al,'$'
+        je FIN
+
+        inc si
+        jmp INICIO
+
+    FIN:
+        mov ah,arreglo[di]
+        cmp ah,'$'
+        je FIN2
+    
+        mov nombresIdentificadores[si],ah
+
+        inc di
+        inc si
+        jmp FIN
+    
+    FIN2:
+        mov nombresIdentificadores[si],186 ;signo rarito
+
+    pop di
+    pop ax
+    pop si
+endm
+
+;=========================== FICHEROS ===================
+abrirF macro ruta,handle
+    push dx
+    xor ax,ax
+    mov ah,3dh
+    mov al,010b
+    lea dx,ruta
+    int 21h
+    mov handle,ax
+    jc ErrorAbrir
+    pop dx
+endm
+
+leerF macro numbytes,buffer,handle
+    push bx
+	mov ah,3fh
+	mov bx,handle
+	mov cx,numbytes
+	lea dx,buffer
+	int 21h
+	jc ErrorLeer
+    pop bx
+endm
+
+crearF macro ruta, handle
+    push ax
+    push dx
+    xor ax,ax
+    mov ah,3ch
+    mov cx,00h
+    lea dx, ruta
+    int 21h
+    mov handle,ax
+    jc ErrorCrear
+    pop dx
+    pop ax
+endm
+
+escribirF macro handle, numBytes, buffer
+    push bx
+    push dx
+    mov ah,40h
+    mov bx,handle
+    mov cx,numBytes
+    lea dx,buffer
+    int 21h
+    jc ErrorEscribir
+    pop dx
+    pop bx
+endm
+
+getRuta macro buffer
+    LOCAL INICIO,FIN
+    push si
+    xor si,si
+        INICIO:
+            getChar
+            cmp al,0dh
+            je FIN
+            mov buffer[si],al
+            inc si
+            jmp INICIO
+        FIN:
+            mov buffer[si],00h
+            inc si
+            mov buffer[si],00h
+            pop si
+endm
+
+cerrarF macro handle
+    push bx
+    xor ax,ax
+	mov ah,3eh
+	mov bx,handle
+	int 21h
+	jc ErrorCerrar
+    pop bx
+endm
+
+borrarF macro ruta
+    mov ah,41h
+    lea dx,ruta
+    int 21h
+endm
+
+escribirA macro arreglo, handle
+    local INICIO, FIN
+    
+    push si
+    xor si,si
+
+    INICIO:
+        cmp arreglo[si],'$'
+        je FIN
+
+        inc si
+        jmp INICIO
+
+    FIN:
+        mov cx,si
+        escribirF handle, cx, arreglo
+
+
+    pop si
+endm
+
+saveUsuarios macro
+    borrarF answers
+    mov handle,00h
+    crearF answers, handle
+    abrirF answers, handle
+    escribirF handle, sizeof usuarios, usuarios
+    escribirF handle, sizeof contrasenas, contrasenas
+    cerrarF handle
+endm
+
+getUsuarios macro
+    mov handle,00h
+    abrirF answers, handle
+    leerF sizeof usuarios, usuarios, handle
+    leerF sizeof contrasenas, contrasenas, handle
+    cerrarF handle
+endm
+
+;==================== COMPARACIONES ========================
+comparar macro actual, molde
+    local INICIO, FIN
+
+    push si
+    xor si,si
+    xor ax,ax
+    tamanoArr molde
+
+    INICIO:
+        mov bh,actual[si]
+        cmp bh,molde[si]
+        jne FIN
+        inc si
+        loop INICIO
+        mov ah,1b
+    
+    FIN:
+        mov al,0b
+
+    pop si      
+    ;si son iguales, ah es 1b si no, al,0b
+endm
+
+tamanoArr macro actual
+    local INICIO, FIN
+    push si
+    xor si,si
+    
+    INICIO:
+        cmp actual[si],'$'
+        je FIN
+        inc si
+        jmp INICIO
+    
+    FIN:
+        inc si
+        mov cx,si
+
+    pop si
+endm
+
+pasarArr macro actual, otro
+    local INICIO
+    push si
+    mov si,00h
+    tamanoArr actual
+
+    INICIO:
+        mov bh,actual[si]
+        mov otro[si],bh
+        inc si
+        loop INICIO
+
+    pop si
+endm
+
+pasarPadre macro
+    local INICIO,FIN
+    push si
+    push dx
+
+    xor si,si
+    inc si
+
+    INICIO:
+        cmp si,65
+        je FIN
+        
+        mov dl,arregloAux[si]
+        dec si
+        mov nombrePadre[si],dl
+        inc si
+        inc si
+        jmp INICIO
+    
+    FIN:
+        mov nombrePadre[si],'$'
+        pop dx
+        pop si
+endm
---
+++ PF/main.asm
@@ -0,0 +1,233 @@
+include macros.asm
+
+.model small ;declaracion del programa
+.stack ;segmento de stack
+.data ;segmento de datos
+;db -> dato byte -> 8 bits
+;dw -> dato word -> 16 bits
+;dd -> doble word -> 32 bits
+nombrePadre db 72 dup('$')
+arregloAux db 70 dup('$'),10,13
+usuarioActual db 70 dup('$'),10,13
+ordenados word 100 dup(00h),'$'
+variable word ?
+auxWord word 34 dup('$'),10,13
+resultadosTmp word 34 dup('$'),10,13
+contrasenas word 100 dup(00h),'$'
+usuarios word 100 dup(00h),'$'
+nombresIdentificadores db 3000 dup('$')
+bufferLectura db 30000 dup('$')
+bufferEscritura db 200 dup('$')
+resultados db 1000 dup('$')
+handleFichero dw ?
+handle2 dw ?
+handle dw ?
+rutaAux db 'nuevo.arq',00h,00h
+ruta2 db 'nuev2.arq',00h,00h
+answers db 'users.rep',00h,00h
+
+exit db 'EXIT','$'
+noexisteMsj db 'La operaci',162,'n que ingresaste no existe','$'
+repGenerado db 10,10,13,32,173,173,' Reporte Generado !!','$'
+userAdmin db 'adminAI','$$'
+contrAdmin db '4321','$$'
+
+
+encab db 10,10,13,'UNIVERSIDAD DE SAN CARLOS DE GUATEMALA',10,13,'FACULTAD DE INGENIERIA',10,13,'CIENCIAS Y SISTEMAS',10,13,'ARQUITECTURA DE COMPUTADORES Y ENSAMBLADORES 1','$'
+enc db 10,10,13,'NOMBRE: ASUNCION MARIANA SIC SOR',10,13,'CARNET: 201504051',10,13,'SECCION: A','$'
+encab2 db 10,13,10,13,'1. Ingresar',10,13,'2. Registrar',10,13,'3. Salir',10,13,10,'::Escoja una opci',162,'n  ','$'
+opcAdmin db 10,13,10,13,'1. Top 10 Puntos',10,13,'2. Top 10 Tiempo',10,13,'3. Salir',10,13,10,'::Escoja una opci',162,'n  ','$'
+msjOpc1 db 10,10,13,9,'-----------------------  INGRESAR  -----------------------',10,13,'$'
+msjOpc2 db 10,10,13,9,'-----------------------  REGISTRAR  -----------------------',10,13,'$'
+msjOpc3 db 10,10,13,9,'-----------------------  ADIOS  -----------------------',10,13,'$'
+msjOpc4 db 10,10,13,9,'-----------------------  TOP 10 PUNTOS  -----------------------',10,13,'$'
+msjOpc5 db 10,10,13,9,'-----------------------  TOP 10 TIEMPO  -----------------------',10,13,'$'
+msjOpc6 db 10,10,13,9,'-----------------------  INICIO JUEGO  -----------------------',10,13,'$'
+saltoln db 10,13,'$'
+console db 10,13,32,32,175,32,'$'
+;; mensajees :D
+msmError1 db 0ah,0dh,'Error al abrir archivo','$'
+msmError2 db 0ah,0dh,'Error al leer archivo','$'
+msmError3 db 0ah,0dh,'Error al crear archivo','$'
+msmError4 db 0ah,0dh,'Error al escribir archivo','$'
+msmError5 db 0ah,0dh,'Error al cerrar archivo','$'
+errorComando db ' ',173,173,' Error, ingresa un comando v',160,'lido !!','$'
+ingreseRuta db 10,13, '::Ingresa nombre archivo a cargar  ','$'
+ingreseUser db 10,13,'::Ingresa tu usuario  ','$'
+welcome db 10,10,13,173,173,' Bienvenido ','$'
+ennd db ' !!',10,13,'$'
+ingreseCont db 10,13,'::Ingresa tu contrase',164,'a  ','$'
+userNoValido db 10,13,32,173,173,' Usuario No Existe !!','$'
+userRegistrado db 10,13,32,173,173,' Usuario Registrado !!',10,13,'$'
+userNoValido2 db 10,13,32,173,173,' Usuario Ya Existe !!',10,13,'$'
+contraNoValida db 10,13,32,173,173,' Contrase',164,'a Inv',160,'lida !!','$'
+contraNumerica db 10,13,32,173,173,' Contrase',164,'a debe ser num',130,'rica !!','$'
+
+
+saveSuccess db 10,13, ' ',173,173,' Archivo guardado con ',130,'xito !!','$'
+loadSuccess db 10,13, ' ',173,173,' Archivo cargado con ',130,'xito !!','$'
+
+.code	
+	
+main proc
+    mov dx,@data
+    mov ds,dx
+
+	MenuPrincipal:
+        cleanArr usuarioActual
+		print encab
+		print enc
+		print encab2
+		getChar
+		cmp al,'1'
+		je INGRESAR
+		cmp al,'2'
+		je REGISTRAR
+		cmp al,'3'
+		je SALIR
+		jmp MenuPrincipal
+
+	
+	INGRESAR:
+        cleanArr arregloAux
+        ConvertirSumaAscii userAdmin
+        colocarRespuesta usuarios
+        ConvertirSumaAscii contrAdmin
+        colocarRespuesta contrasenas
+		print msjOpc1
+        print ingreseUser
+        ObtenerTexto arregloAux
+        ConvertirSumaAscii arregloAux
+        searchID
+        push dx
+        cmp dx,0
+        je USERINVALIDO
+
+        ;usuario valido
+        pasarArr arregloAux,usuarioActual
+        print welcome
+        print arregloAux
+        print ennd
+        cleanArr arregloAux
+        print ingreseCont
+        ObtenerPwd arregloAux
+        ConvertirSumaAscii arregloAux
+        pop dx
+        cmp dx,ax
+        je VALIDOS
+
+        print contraNoValida
+        getChar
+		jmp MenuPrincipal
+    
+    USERINVALIDO:
+        print userNoValido
+        getChar
+        jmp MenuPrincipal
+    
+    VALIDOS:
+        comparar usuarioActual,userAdmin
+        cmp ah,0b
+        je NORMALUSER
+
+        print opcAdmin
+        getChar
+        cmp al,'1'
+        je TOP10PUNTOS
+        cmp al,'2'
+        je TOP10TIEMPO
+        
+        jmp MenuPrincipal
+
+    NORMALUSER:
+        print msjOpc6
+        jmp MenuPrincipal
+    
+    TOP10PUNTOS:
+        print msjOpc4
+        jmp MenuPrincipal
+
+    TOP10TIEMPO:
+        print msjOpc5
+        jmp MenuPrincipal
+    
+    REGISTRAR:
+        print msjOpc2
+        print ingreseUser
+        cleanArr arregloAux
+        ObtenerTexto arregloAux
+        ConvertirSumaAscii arregloAux
+        searchID
+        push ax
+        cmp dx,0
+        jne YAEXISTE
+        
+        pop ax
+        colocarRespuesta usuarios
+    
+    INGCONTRASE:
+        print ingreseCont
+        cleanArr arregloAux
+        ObtenerPwd arregloAux
+        cmp ch,00h
+        je INVALISENA
+
+        ConvertirSumaAscii arregloAux
+        colocarRespuesta contrasenas
+        print userRegistrado
+        
+
+        jmp MenuPrincipal
+
+    INVALISENA:
+        jmp INGCONTRASE
+
+    YAEXISTE:
+        print userNoValido2
+        getChar
+        jmp REGISTRAR
+    
+    
+	SALIR:
+		print msjOpc3
+		mov ah,4ch
+		int 21h
+    	    
+    ErrorAbrir:
+		print msmError1
+		getChar
+		jmp MenuPrincipal
+
+	ErrorLeer:
+		print msmError2
+		getChar
+		jmp MenuPrincipal
+
+	ErrorCrear:
+		print msmError3
+		getChar
+		jmp MenuPrincipal
+
+	ErrorEscribir:
+		print msmError4
+		getChar
+		jmp MenuPrincipal
+
+	ErrorCerrar:
+		print msmError5
+		getChar
+		jmp MenuPrincipal
+
+main endp
+
+
+DISP PROC
+    MOV DL,BH      ; Since the values are in BX, BH Part
+    ADD DL,30H     ; ASCII Adjustment
+
+    MOV al,BL      ; BL Part 
+    ADD al,30H     ; ASCII Adjustment
+    RET
+DISP ENDP      ; End Disp Procedure
+
+end
